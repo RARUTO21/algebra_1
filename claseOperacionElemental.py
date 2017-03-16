841de396cf4bce7109a3da7849040aaf4cf282ac
mena97villalobos <mena97villalobos@gmail.com>
2017-03-15 20:51:43 -0600

    Inicio

---
+++ claseOperacionElemental.py
@@ -0,0 +1,21 @@
+class TipoA:
+    def __init__(self, tipo):
+        if(tipo == 1):
+            self.nombre = "Tipo A"
+            self.filaA = 0
+            self.filaB = 0
+
+
+class TipoB:
+    def __init__(self):
+            self.nombre = "Tipo B"
+            self.filaC = 0
+            self.constanteA = 0
+
+
+class TipoC:
+    def __init__(self):
+            self.nombre = "Tipo C"
+            self.filaD = 0
+            self.filaE = 0
+            self.constanteB = 0
---
+++ crearSecuenciaResultados.py
@@ -0,0 +1,22 @@
+from claseOperacionElemental import *
+
+def writeToFile(text):
+    fileHandle = open("Resultados.txt", "a+")
+    fileHandle.write(text + "\n")
+    fileHandle.close()
+
+def guardarResultados(matriz, operacionElemental):
+    tmp = ""
+    if(type(operacionElemental) is TipoA):
+           tmp += str(matriz)+ " Operación elemental del tipo A, se cambio la fila "+str(
+               operacionElemental.filaA)+" por fila"+str(operacionElemental.filaA)
+    elif(type(operacionElemental) is TipoB):
+        tmp += str(matriz) + " Operación elemental del tipo B, se multiplicó la fila " + str(
+            operacionElemental.filaC) + " por constante" + str(operacionElemental.cosntanteA)
+    elif (type(operacionElemental) is TipoC):
+        tmp += str(matriz) + " Operación elemental del tipo C, se multiplicó la fila " + str(
+            operacionElemental.filaD) + " por fila " + str(operacionElemental.filaE)+" por constante: "+str(
+            operacionElemental.constanteB)
+    else:
+        tmp+="[[-1]] Error"
+    writeToFile(tmp)
